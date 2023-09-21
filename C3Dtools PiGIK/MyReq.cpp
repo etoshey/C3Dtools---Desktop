cd6da666c1e32913381f5d60a361b32d4d0012e6
soroosh bagheri <soroosh.b.k@gmail.com>
2023-09-21 15:08:36 +0330

    extraxct data and generate files

--- C3Dtools PiGIK/MyReq.cpp
+++ C3Dtools PiGIK/MyReq.cpp
@@ -13,6 +13,9 @@
 #include <Poco/URI.h>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
 
 using namespace Poco;
 using namespace Poco::Net;
@@ -83,36 +86,268 @@ std::string MyReq::SendReq(std::string url, std::map<std::string, std::string> *
 	
 }
 
-std::string MyReq::upload(std::string url, std::map<std::string, bool>* parameter , std::string file_path)
+std::string MyReq::upload(std::string url, std::map<std::string, std::string>* parameter , std::string file_path, std::vector<std::string> * log)
 {
-	// Create a URI
-	URI uri(url);
 
-	HTTPRequest request(HTTPRequest::HTTP_POST, uri.getPath(), HTTPMessage::HTTP_1_1);
-	HTMLForm form;
-	form.setEncoding(HTMLForm::ENCODING_MULTIPART);
-	std::map<std::string, bool>::iterator it = parameter->begin();
-	while (it != parameter->end())
+	try
 	{
-		if (it->second) {
-			form.set(it->first, "TRUE");
+		
+		
+
+		std::filesystem::path p(file_path);
+		std::filesystem::path dir = p.parent_path();
+		std::filesystem::path file_name = p.filename();
+
+
+		// Create a URI
+		URI uri(url);
+
+		HTTPRequest request(HTTPRequest::HTTP_POST, uri.getPath(), HTTPMessage::HTTP_1_1);
+		HTMLForm form;
+		form.setEncoding(HTMLForm::ENCODING_MULTIPART);
+		std::map<std::string, std::string>::iterator it = parameter->begin();
+		while (it != parameter->end())
+		{			
+			form.set(it->first, it->second);
+			++it;
+		}
+		form.addPart("file", new FilePartSource(file_path));
+		form.prepareSubmit(request);
+
+		HTTPClientSession* httpSession = new HTTPClientSession(uri.getHost(), uri.getPort());
+		httpSession->setTimeout(Poco::Timespan(60, 0));
+		form.write(httpSession->sendRequest(request));
+
+		Poco::Net::HTTPResponse res;
+		std::istream& s = httpSession->receiveResponse(res);
+		std::ostringstream outStringStream;
+		outStringStream << s.rdbuf();
+		std::string res_txt = outStringStream.str();
+		Poco::JSON::Parser parser;
+		auto result = parser.parse(res_txt);
+		Poco::JSON::Object obj = *result.extract<Poco::JSON::Object::Ptr>();
+		std::string status = obj.get("status");
+		if (status == "OK") {
+			std::string data = obj.get("results");
+			auto main_results = parser.parse(data);
+			Poco::JSON::Object Main_data = *main_results.extract<Poco::JSON::Object::Ptr>();
+
+			//-------------------- TRC  --------------------------//
+			std::map<std::string, std::string>::iterator check_trc = parameter->find("TRC");
+			if (check_trc->second == "TRUE") {
+				std::string trc_data = Main_data.get("trc_result");
+				std::vector<std::string> trc_output;
+				ExtractTrc(&trc_data, &trc_output, log);
+				MakeDir(dir.string() + "\\C3Dtools");
+				if (WriteFile(dir.string() + "\\C3Dtools\\" + file_name.string() + ".trc", &trc_output, false)) {
+					//file is generated succssfully
+					log->push_back("Exporting TRC is done.");
+				}
+				else {
+					log->push_back("Exporting TRC Failed.");
+				}
+
+			}
+			//-------------------- MOT  --------------------------//
+			std::map<std::string, std::string>::iterator check_mot = parameter->find("MOT");
+			if (check_mot->second == "TRUE") {
+				std::string mot_data = Main_data.get("mot_result");
+				std::vector<std::string> mot_output;
+				ExtractMot(&mot_data, &mot_output,log);
+				MakeDir(dir.string() + "\\C3Dtools");
+				if (WriteFile(dir.string() + "\\C3Dtools\\" + file_name.string() + ".mot", &mot_output, false)) {
+					//file is generated succssfully
+					log->push_back("Exporting MOT is done.");
+				}
+				else {
+					log->push_back("Exporting MOT Failed.");
+				}
+			}
+			//-------------------- ASCII POINT  --------------------------//
+			std::map<std::string, std::string>::iterator check_ascii_point = parameter->find("ASCII_Points");
+			if (check_ascii_point->second == "TRUE") {
+				std::string ascii_point_data = Main_data.get("ascii_point_result");
+				std::vector<std::string> ascii_points_output;
+				ExtractAscii(&ascii_point_data, &ascii_points_output, log);
+				MakeDir(dir.string() + "\\C3Dtools");
+				if (WriteFile(dir.string() + "\\C3Dtools\\" + file_name.string() + ".txt", &ascii_points_output, false)) {
+					//file is generated succssfully
+					log->push_back("Exporting ASCII - Points is done.");
+				}
+				else {
+					log->push_back("Exporting ASCII - Points Failed.");
+				}
+			}
+			//-------------------- ASCII Analog  --------------------------//
+			std::map<std::string, std::string>::iterator check_ascii_analog = parameter->find("ASCII_Analog");
+			if (check_ascii_analog->second == "TRUE") {
+				std::string ascii_analog_data = Main_data.get("ascii_analog_result");
+				std::vector<std::string> ascii_analoge_output;
+				ExtractAscii(&ascii_analog_data, &ascii_analoge_output, log);
+				MakeDir(dir.string() + "\\C3Dtools");
+				if (WriteFile(dir.string() + "\\C3Dtools\\" + file_name.string() + ".txt", &ascii_analoge_output, false)) {
+					//file is generated succssfully
+					log->push_back("Exporting ASCII - Analog is done.");
+				}
+				else {
+					log->push_back("Exporting ASCII - Analog Failed.");
+				}
+			}
+
+
 		}
 		else {
-			form.set(it->first, "FALSE");
+			return obj.get("error_msg");
 		}
-		++it;
+
+		return std::string();
 	}
-	form.addPart("file", new FilePartSource(file_path));
-	form.prepareSubmit(request);
-
-	HTTPClientSession* httpSession = new HTTPClientSession(uri.getHost(), uri.getPort());
-	httpSession->setTimeout(Poco::Timespan(60, 0));
-	form.write(httpSession->sendRequest(request));
-
-	Poco::Net::HTTPResponse res;
-	std::istream& is = httpSession->receiveResponse(res);
-	Poco::StreamCopier::copyStream(is, std::cout);
-
-
-	return std::string();
+	catch (const std::exception&)
+	{
+		log->push_back( " FAILED !!");
+	}
 }
+
+bool MyReq::WriteFile(std::string path, std::vector<std::string>* data, bool append)
+{
+	std::fstream file;
+
+	//if exist 
+	if (std::filesystem::exists(path)) {
+
+		// remove file
+		std::filesystem::remove(path);
+	}
+
+	file.open(path, std::ios_base::out);
+	if (!file.is_open())
+	{
+		return false;
+	}
+	
+	for (auto i = data->begin(); i != data->end(); i++)
+	{
+		file << *i;
+	}
+	file.close();
+
+	return true;
+}
+
+void MyReq::ExtractTrc(std::string* trc_data, std::vector<std::string>* output, std::vector<std::string>* log)
+{
+	Poco::JSON::Parser parser;
+	auto trc_results = parser.parse(*trc_data);
+	Poco::JSON::Object trc_obj = *trc_results.extract<Poco::JSON::Object::Ptr>();
+	try
+	{
+		std::string trc_success_msg = trc_obj.get("success_msg");
+		std::string _trc_json = trc_obj.get("json");
+		auto trc = parser.parse(_trc_json);
+		Poco::JSON::Object trc_main = *trc.extract<Poco::JSON::Object::Ptr>();
+		std::string _trc_h1 = trc_main.get("h1");
+		std::string _trc_h2 = trc_main.get("h2");
+		std::string _trc_h3 = trc_main.get("h3");
+		std::string _trc_h4 = trc_main.get("h4");
+		std::string _trc_h5 = trc_main.get("h5");
+		output->push_back(_trc_h1);
+		output->push_back(_trc_h2);
+		output->push_back(_trc_h3);
+		output->push_back(_trc_h4);
+		output->push_back(_trc_h5);
+
+		Poco::JSON::Array::Ptr _trc_array = trc_main.getArray("data");
+		Poco::Dynamic::Array da = *_trc_array;
+		for (int i = 0; i < da.size(); i++)
+		{
+			output->push_back(da[i]);
+		}
+	}
+	catch (const std::exception&)
+	{
+		std::string mot_error_msg = trc_obj.get("error_msg");
+		log->push_back("Failed to extract TRC data *");
+
+	}
+
+}
+
+void MyReq::ExtractMot(std::string* mot_data, std::vector<std::string>* output , std::vector<std::string>* log)
+{
+	Poco::JSON::Parser parser;
+	auto mot_results = parser.parse(*mot_data);
+	Poco::JSON::Object mot_obj = *mot_results.extract<Poco::JSON::Object::Ptr>();
+	// Success || Failed
+	try
+	{
+		std::string mot_success_msg = mot_obj.get("success_msg");
+		std::string _mot_json = mot_obj.get("json");
+		auto mot = parser.parse(_mot_json);
+		Poco::JSON::Object mot_main = *mot.extract<Poco::JSON::Object::Ptr>();
+		std::string _mot_h1 = mot_main.get("h1");
+		std::string _mot_h2 = mot_main.get("h2");
+		output->push_back(_mot_h1);
+		output->push_back(_mot_h2);
+
+		Poco::JSON::Array::Ptr _mot_array = mot_main.getArray("data");
+		Poco::Dynamic::Array da = *_mot_array;
+		for (int i = 0; i < da.size(); i++)
+		{
+			output->push_back(da[i]);
+		}
+	}
+	catch (const std::exception&)
+	{
+		std::string mot_error_msg = mot_obj.get("error_msg");
+		log->push_back("Failed to extract Mot data *");
+		
+	}
+
+}
+
+void MyReq::ExtractAscii(std::string* _data, std::vector<std::string>* output, std::vector<std::string>* log)
+{
+	Poco::JSON::Parser parser;
+	auto _results = parser.parse(*_data);
+	Poco::JSON::Object _obj = *_results.extract<Poco::JSON::Object::Ptr>();
+	try
+	{
+
+		std::string _success_msg = _obj.get("success_msg");
+		std::string _json = _obj.get("json");
+		auto ascii = parser.parse(_json);
+		Poco::JSON::Object ascii_main = *ascii.extract<Poco::JSON::Object::Ptr>();
+		std::string _ascii_h1 = ascii_main.get("h1");
+		std::string _ascii_h2 = ascii_main.get("h2");
+		output->push_back(_ascii_h1);
+		output->push_back(_ascii_h2);
+
+		Poco::JSON::Array::Ptr _ascii_array = ascii_main.getArray("data");
+		Poco::Dynamic::Array da = *_ascii_array;
+		for (int i = 0; i < da.size(); i++)
+		{
+			output->push_back(da[i]);
+		}
+	}
+	catch (const std::exception&)
+	{
+		std::string mot_error_msg = _obj.get("error_msg");
+		log->push_back("Failed to extract ASCII data *");
+	}
+}
+
+bool MyReq::MakeDir(std::string path)
+{
+	const char* _path = path.c_str();
+	std::filesystem::path dir(_path);
+	if (std::filesystem::create_directory(dir))
+	{
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
--- C3Dtools PiGIK/MyReq.h
+++ C3Dtools PiGIK/MyReq.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <map>
+#include <vector>
+
 
 
 
@@ -9,13 +11,19 @@ class MyReq
 public:
 	MyReq();
 	std::string SendReq(std::string url, std::map<std::string, std::string>* parameter);
-	std::string upload(std::string url, std::map<std::string, bool>* parameter, std::string file_path);
-
+	std::string upload(std::string url, std::map<std::string, std::string>* parameter, std::string file_path, std::vector<std::string>* log);
 
 
 
 
 private:
+	bool WriteFile(std::string path, std::vector<std::string>* data, bool append);
+	void ExtractTrc(std::string* trc_data, std::vector<std::string>* output, std::vector<std::string>* log);
+	void ExtractMot(std::string* mot_data, std::vector<std::string>* output, std::vector<std::string>* log);
+	void ExtractAscii(std::string* mot_data, std::vector<std::string>* output, std::vector<std::string>* log);
+	bool MakeDir(std::string path);
+
+
 	
 };
 
